6d44ca878407e4cb19d178fe6365967475970309
Paul Kögel <i.paul.koegel@gmail.com>
2017-12-11 00:19:43 +0100

    + `yarn add -D eslint babel-eslint eslint-config-semistandard-react eslint-plugin-semistandard-react` + add .eslintrc.js + `yarn eslintrc src --fix`

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: "semistandard-react",
+  rules: {
+    "no-useless-return": "off",
+    quotes: ["error", "single"],
+    "semistandard-react/prop-types": "off",
+    "space-before-function-paren": "error",
+    "space-infix-ops": "off"
+  }
+};
